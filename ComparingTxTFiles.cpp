b7988f9d7b991fd4d92fa5d3855081e220434ad0
Novl <Novelz@yandex.ru>
2015-10-21 02:29:05 +0300

    Text without newlines

--- ComparingTxTFiles.cpp
+++ ComparingTxTFiles.cpp
@@ -1,17 +1,29 @@
 ﻿#include <iostream>
 #include <string>
 #include <fstream>
+#include <clocale>
 
 using namespace std;
 
-string FileName1, FileName2, FileNameResult;
+string support;
+string FileName1, FileName2, FileNameResult, OutFlag;
 fstream File1, File2;
 fstream FileResult;
-char OutFlag;
 
 string StrFile1, StrFile2;
-long long LineNum;
+long long LineNumFile1, LineNumFile2;
+bool f1, f2;
 
+
+void HelpMenu()
+{
+    cout<<" Help "<<endl;
+    cout<<" 1 or 2   - to print only 1 lane from file #"<<endl;
+    cout<<" 1s or 2s - to print 1 lane and skip another"<<endl;
+    cout<<" s1 or s1 - to just skip selected lane"<<endl; 
+    cout<<" 1a or 2a - to read # until they equal or end of file"<<endl;
+    
+}
 void readFileName1(string &str)
 {
     cout<<" Enter File 1 name"<<endl;
@@ -32,6 +44,7 @@ void readFileNameResult(string &str)
 
 int main(int args, char* argv[], char* envp[])
 {
+    setlocale(LC_ALL, "Russian");
     if (args==1)
     {
         readFileName1(FileName1);
@@ -64,48 +77,126 @@ int main(int args, char* argv[], char* envp[])
     File2.open(FileName2.c_str());
     FileResult.open(FileNameResult.c_str(), fstream::out);
     
-    LineNum=1;
+    
+    
+    // ------ MAIN PART -------
+    LineNumFile1 = LineNumFile1 = 1;
     getline(File1, StrFile1, '\n');
     getline(File2, StrFile2, '\n');
     while (!File1.eof() || !File2.eof())
     {
         while (StrFile1.compare(StrFile2)==0 && !File1.eof() && !File2.eof())
         {
-            cout<<"read "<<LineNum<<" line"<<endl;
-            LineNum++;
-            getline(File1, StrFile1, '\n');
-            getline(File2, StrFile2, '\n');
+            //cout<<"read "<<LineNum<<" line"<<endl;
             FileResult<<StrFile1<<endl;
+            
+            do {getline(File1, StrFile1, '\n'); LineNumFile1++;} while (StrFile1.compare(" ")==0 || StrFile1.empty());
+            do {getline(File2, StrFile2, '\n'); LineNumFile2++;} while (StrFile2.compare(" ")==0 || StrFile2.empty());
         }
         if (!File1.eof() && !File2.eof())
         {
-            cout<<"Difference in line "<<LineNum<<endl;
-            cout<<"File 1:"<<endl<<StrFile1<<endl<<endl;
-            cout<<"File 2:"<<endl<<StrFile2<<endl<<endl;
-        
-            OutFlag = getchar();
-            if (OutFlag == '1')
+            f1 = (StrFile1.size() < StrFile2.size()) && (StrFile2.find(StrFile1) == 0);
+            f2 = (StrFile1.size() > StrFile2.size()) && (StrFile1.find(StrFile2) == 0);
+            if (f1)
             {
-                while (StrFile1.compare(StrFile2)!=0 && !File1.eof())
-                {
-                    getline(File1, StrFile1, '\n');
-                    FileResult<<StrFile1<<endl;
-                }
+                getline(File1, support, '\n');
+                LineNumFile1++;
+                if (StrFile2[StrFile1.size()]==' ' && support[0]!=' ') StrFile1+=" ";
+                StrFile1+=support;
             }
             else
-                if (OutFlag == '2')
-                    while (StrFile1.compare(StrFile2)!=0 && !File2.eof())
-                    {
-                        getline(File2, StrFile2, '\n');
-                        FileResult<<StrFile2<<endl;
-                    }
+            if (f2)
+            {
+                getline(File2, support, '\n');
+                LineNumFile2++;
+                if (StrFile1[StrFile2.size()]==' ' && support[0]!=' ') StrFile2+=" ";
+                StrFile2+=support;
+            }
+            else
+            {
+                cout<<endl<<"///////////////////"<<endl;
+                cout<<"Difference in lines "<<endl;
+                cout<<"FILE1 str: "<<LineNumFile1<<endl;
+                cout<<"File 1:"<<endl<<StrFile1<<endl<<endl;
+                cout<<"FILE2 str: "<<LineNumFile2<<endl;
+                cout<<"File 2:"<<endl<<StrFile2<<endl<<endl;
+                
+                HelpMenu();
+                
+                getline(cin, OutFlag);
+                if (OutFlag.compare("1")==0)
+                {
+                    FileResult<<StrFile1<<endl;
+                    getline(File1, StrFile1, '\n');
+                    LineNumFile1++;
+                }
                 else
-                    break;
+                    if (OutFlag.compare("2")==0)
+                        {
+                            FileResult<<StrFile2<<endl;
+                            getline(File2, StrFile2, '\n');
+                            LineNumFile2++;
+                        }
+                        else
+                            if (OutFlag.compare("1s")==0)
+                            {
+                                FileResult<<StrFile1<<endl;
+                                getline(File1, StrFile1, '\n');
+                                getline(File2, StrFile2, '\n');
+                                LineNumFile1++;
+                                LineNumFile2++;
+                            }
+                            else
+                                if (OutFlag.compare("2s")==0)
+                                    {
+                                        FileResult<<StrFile2<<endl;
+                                        getline(File1, StrFile1, '\n');
+                                        getline(File2, StrFile2, '\n');
+                                        LineNumFile1++;
+                                        LineNumFile2++;
+                                    }
+                                    else
+                                        if (OutFlag.compare("s1")==0)
+                                        {
+                                            getline(File1, StrFile1, '\n');
+                                            LineNumFile1++;
+                                        }
+                                        else
+                                            if (OutFlag.compare("s2")==0)
+                                            {    
+                                                getline(File2, StrFile2, '\n');
+                                                LineNumFile2++;
+                                            }
+                                            else
+                                                if (OutFlag.compare("1a")==0)
+                                                {
+                                                    while (StrFile1.compare(StrFile2)!=0 && !File1.eof())
+                                                    {
+                                                        FileResult<<StrFile1<<endl;
+                                                        getline(File1, StrFile1, '\n');
+                                                        LineNumFile1++;
+                                                    }
+                                                }
+                                                else
+                                                    if (OutFlag.compare("2a")==0)
+                                                    {    
+                                                        while (StrFile1.compare(StrFile2)!=0 && !File2.eof())
+                                                        {
+                                                            FileResult<<StrFile2<<endl;
+                                                            getline(File2, StrFile2, '\n');
+                                                            LineNumFile2++;
+                                                        }
+                                                    }
+                                                    else
+                                                        goto short_end;
+            }
         }
     }    
     
     
-    cout<<"All done"<<endl<<"Goodbye"<<endl;
+    cout<<"All done"<<endl;
+    short_end:
+    cout<<"Goodbye"<<endl;
     getchar();
     File1.close();
     File2.close();
